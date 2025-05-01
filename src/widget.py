3cae1259f53d2c5cc5ea95f840b79524fad9d5b9
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-05-01 23:13:14 +0300

    добавлена функция изменения формата даты

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 import masks
 
 
+
 def mask_account_card(card_or_account_info: str) -> str:
-    
+    """Функция маскирующая номера карты или счета"""
     card_or_account_list = card_or_account_info.rsplit(maxsplit=1)
     
     if len(card_or_account_list[1]) == 16:
@@ -11,6 +14,9 @@ def mask_account_card(card_or_account_info: str) -> str:
         return card_or_account_list[0] + " " + masks.get_mask_account(int(card_or_account_list[1]))
 
 
-print(mask_account_card('Visa Platinum 8990922113665229'))
+def get_date(date: str) -> str:
+    """Приводит строку с датой к формату 'ДД.ММ.ГГГГ'"""
+    format_date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
 
-    
+    return format_date.strftime("%d.%m.%Y")
+
