d41ef4d980d4b2f11365c64882f8df656070681f
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-05-03 14:42:38 +0300

    Исправления после проверки линтерами

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,9 @@
 from typing import List
 
 
-def filter_by_state(my_list: List[dict], filter_parameter: str='EXECUTED') -> List[dict]:
+def filter_by_state(my_list: List[dict], filter_parameter: str = "EXECUTED") -> List[dict]:
     """Функция фильтрует список по указанному значению параметра state"""
-    new_list =[]
+    new_list = []
 
     for elem in my_list:
         if elem["state"] == filter_parameter:
@@ -12,10 +12,9 @@ def filter_by_state(my_list: List[dict], filter_parameter: str='EXECUTED') -> Li
     return new_list
 
 
-def sort_by_date(my_list: List[dict], reverse_state=True) -> List[dict]:
+def sort_by_date(my_list: List[dict], reverse_state: bool = True) -> List[dict]:
     """Функция сортирует список со словарями по ключу data"""
 
     new_list = sorted(my_list, key=lambda x: x["date"], reverse=reverse_state)
-
 
     return new_list
