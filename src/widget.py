9cefa3a22a31e694aabe3f3c9d5928a8a5744b87
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-05-04 22:56:10 +0300

    изменена функция mask_accounnt_card, добавлены тесты для нее

--- src/widget.py
+++ src/widget.py
@@ -6,10 +6,18 @@ from masks import get_mask_account, get_mask_card_number
 def mask_account_card(card_or_account_info: str) -> str:
     """Функция маскирующая номера карты или счета"""
     card_or_account_list = card_or_account_info.rsplit(maxsplit=1)
-    if len(card_or_account_list[1]) == 16:
-        return f"{card_or_account_list[0]} {get_mask_card_number(int(card_or_account_list[1]))}"
+    # Проверяем что номер счет или кары состоит из цифр
+    if int(card_or_account_list[1]):
+        # Проверяем что введенная строка не имеет в себе слово "счет" и длина номера равна 16 цифрам
+        if len(card_or_account_list[1]) == 16 and "счет" not in card_or_account_info.lower():
+            return f"{card_or_account_list[0]} {get_mask_card_number(int(card_or_account_list[1]))}"
+        # Проверяем что введенная строка имеет в себе слово "счет" и длина номера равна 20 цифрам
+        elif len(card_or_account_list[1]) == 20 and "счет" in card_or_account_info.lower():
+            return f"{card_or_account_list[0]} {get_mask_account(int(card_or_account_list[1]))}"
+        else:
+            return "Неверный формат, введенных, данных"
     else:
-        return f"{card_or_account_list[0]} {get_mask_account(int(card_or_account_list[1]))}"
+        return "Неверный формат, введенных, данных"
 
 
 def get_date(date: str) -> str:
--- tests/tests_for_code.py
+++ tests/tests_for_code.py
@@ -1,12 +1,39 @@
+import pytest
+
 from src.masks import get_mask_card_number, get_mask_account
 from src.widget import mask_account_card, get_date
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_mask_card():
-    assert get_mask_card_number(7000792289606361) == "7000 79** **** 6361"
+@pytest.fixture()
+def my_card_number():
+    return 7000792289606361
 
 
-def test_mask_account():
-    assert  get_mask_account(73654108430135874305) == "**4305"
+@pytest.fixture()
+def my_account_number():
+    return 73654108430135874305
+
+
+def test_mask_card(my_card_number):
+    assert get_mask_card_number(my_card_number) == "7000 79** **** 6361"
+
+
+def test_mask_account(my_account_number):
+    assert  get_mask_account(my_account_number) == "**4305"
+
+
+@pytest.mark.parametrize("my_string, result_of_masking", [
+    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+    ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+    ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+    ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+    ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+    ("Счет 64686473678894779589", "Счет **9589"),
+    ("Счет 35383033474447895560", "Счет **5560"),
+    ("Счет 73654108430135874305", "Счет **4305"),
+])
+def test_mask_account_card(my_string, result_of_masking):
+    assert mask_account_card(my_string) == result_of_masking
+
 
