5d078531dc91b18c72ebc22ce51e489cb9894200
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-05-04 23:37:38 +0300

    Дополнен тест test_mask_account_card, добавлены изменения в функци get_mask_card_number, get_mask_account, mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,13 @@
 def get_mask_card_number(number_card: int) -> str:
     """Маскирует номер карты"""
-
-    return f"{str(number_card)[:4]} {str(number_card)[4:6]}** **** {str(number_card)[-4:]}"
-
+    if len(str(number_card)) == 16:
+        return f"{str(number_card)[:4]} {str(number_card)[4:6]}** **** {str(number_card)[-4:]}"
+    else:
+        return "Неверный формат, введенных, данных"
 
 def get_mask_account(number_account: int) -> str:
     """Маскирует номер счета"""
-
-    return f"**{str(number_account)[-4:]}"
+    if len(str(number_account)) == 20:
+        return f"**{str(number_account)[-4:]}"
+    else:
+        return "Неверный формат, введенных, данных"
--- src/widget.py
+++ src/widget.py
@@ -5,17 +5,28 @@ from masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(card_or_account_info: str) -> str:
     """Функция маскирующая номера карты или счета"""
+    supported_cards = ["Visa Gold", "Visa Platinum", "Visa Classic", "MasterCard", "Maestro"]
+
+    # Проверяем что во введенной строке есть символы и есть хотя бы 1 пробел
+    if not len(card_or_account_info) or " " not in card_or_account_info:
+        return "Неверный формат, введенных, данных"
+
     card_or_account_list = card_or_account_info.rsplit(maxsplit=1)
-    # Проверяем что номер счет или кары состоит из цифр
-    if int(card_or_account_list[1]):
-        # Проверяем что введенная строка не имеет в себе слово "счет" и длина номера равна 16 цифрам
-        if len(card_or_account_list[1]) == 16 and "счет" not in card_or_account_info.lower():
-            return f"{card_or_account_list[0]} {get_mask_card_number(int(card_or_account_list[1]))}"
-        # Проверяем что введенная строка имеет в себе слово "счет" и длина номера равна 20 цифрам
-        elif len(card_or_account_list[1]) == 20 and "счет" in card_or_account_info.lower():
-            return f"{card_or_account_list[0]} {get_mask_account(int(card_or_account_list[1]))}"
-        else:
-            return "Неверный формат, введенных, данных"
+
+    try:
+        # Проверяем что номер счет или кары состоит из цифр
+        if int(card_or_account_list[1]):
+            # Проверяем что введенная строка не имеет в себе слово "счет" и длина номера равна 16 цифрам
+            if len(card_or_account_list[1]) == 16 and card_or_account_list[0] in supported_cards:
+                return f"{card_or_account_list[0]} {get_mask_card_number(int(card_or_account_list[1]))}"
+            # Проверяем что введенная строка имеет в себе слово "счет" и длина номера равна 20 цифрам
+            elif len(card_or_account_list[1]) == 20 and card_or_account_list[0].lower() == "счет":
+                return f"{card_or_account_list[0]} {get_mask_account(int(card_or_account_list[1]))}"
+            else:
+                return "Неверный формат, введенных, данных"
+
+    except ValueError:
+        return "Неверный формат, введенных, данных"
     else:
         return "Неверный формат, введенных, данных"
 
