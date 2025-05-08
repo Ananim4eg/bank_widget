e6024e44350727d48e9aa58a6dc0a7b12979e174
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-05-08 17:14:49 +0300

    Добавлена новая директория /generators с шаблонами для будующих функций

---
+++ src/generators.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def filter_by_currency(my_list: List[dict]) -> iter:
+    pass
+
+
+def transaction_descriptions(my_list: List[dict]) -> list:
+    pass
+
+
+def card_number_generator(start: int, stop: int) -> str:
+    pass
