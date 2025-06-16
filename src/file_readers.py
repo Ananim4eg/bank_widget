319a6f2db69a1f7c0e1b5364cb82f903b07a7c75
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-06-16 05:20:01 +0300

    rework functions read_xlsx_file and read_csv_file in module src/file_readers.py

--- src/file_readers.py
+++ src/file_readers.py
@@ -1,20 +1,29 @@
 import csv
+
 import pandas as pd
 
 
-def read_csv_file(path_file: str) -> list[dict]:
+def read_csv_file(path_file: str) -> list[dict] | str:
     """Считывает csv файл и возвращает список словарей с транзакциями"""
 
-    with open(path_file, encoding='utf-8') as csv_file:
-        result = csv.DictReader(csv_file, delimiter=';')
-        next(result)
+    try:
+        with open(path_file, encoding='utf-8') as csv_file:
+            result = csv.DictReader(csv_file, delimiter=';')
+            next(result)
 
-        return [row for row in result]
+            return [row for row in result]
+
+    except FileNotFoundError:
+        return 'Файл не найден'
 
 
-def read_xlsx_file(path_file: str) -> list[dict]:
+def read_xlsx_file(path_file: str) -> list[dict] | str:
     """Считывает xlsx файл и возвращает список словарей с транзакциями"""
 
-    excel_data = pd.read_excel(path_file)
+    try:
+        excel_data = pd.read_excel(path_file)
 
-    return list(excel_data.to_dict(orient='records'))
+        return list(excel_data.to_dict(orient='records'))
+
+    except FileNotFoundError:
+        return 'Файл не найден'
---
+++ tests/tests_file_readers.py
@@ -0,0 +1,3 @@
+import pytest
+from src.file_readers import read_xlsx_file,read_csv_file
+
