0ae6711a8de93a8c9c5939f041f47c432e36e8cf
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-06-16 07:09:12 +0300

    add tests for function read_xlsx_file

--- tests/tests_file_readers.py
+++ tests/tests_file_readers.py
@@ -2,6 +2,7 @@ import csv
 from unittest import mock
 from unittest.mock import patch, mock_open, MagicMock
 
+import pandas as pd
 import pytest
 from src.file_readers import read_xlsx_file,read_csv_file
 
@@ -13,6 +14,21 @@ def my_csv_file():
 
 
 def test_read_csv_success(my_csv_file):
+
+    expected = [
+        {
+            'id': '650703',
+            'state': 'EXECUTED',
+            'date': '2023-09-05T11:30:32Z',
+            'amount': '16210',
+            'currency_name': 'Sol',
+            'currency_code': 'PEN',
+            'from': 'Счет 58803664561298323391',
+            'to': 'Счет 39745660563456619397',
+            'description': 'Перевод организации'
+        }
+    ]
+
     mock_file = MagicMock()
     mock_file.__enter__.return_value = mock_file
 
@@ -23,19 +39,6 @@ def test_read_csv_success(my_csv_file):
 
     mocked_open.assert_called_once_with('test.csv', encoding='utf-8')
 
-    expected = [
-    {
-        'id': '650703',
-        'state': 'EXECUTED',
-        'date': '2023-09-05T11:30:32Z',
-        'amount': '16210',
-        'currency_name': 'Sol',
-        'currency_code': 'PEN',
-        'from': 'Счет 58803664561298323391',
-        'to': 'Счет 39745660563456619397',
-        'description': 'Перевод организации'
-    }
-]
     assert result == expected
 
 
@@ -44,3 +47,51 @@ def test_read_csv_file_not_found():
         result = read_csv_file('missing.csv')
 
     assert result == 'Файл не найден'
+
+
+@pytest.fixture()
+def my_dataframe():
+    return pd.DataFrame({
+    'id': ['650703'],
+    'state': ['EXECUTED'],
+    'date': ['2023-09-05T11:30:32Z'],
+    'amount': ['16210'],
+    'currency_name': ['Sol'],
+    'currency_code': ['PEN'],
+    'from': ['Счет 58803664561298323391'],
+    'to': ['Счет 39745660563456619397'],
+    'description': ['Перевод организации']
+})
+
+
+def test_read_xlsx_success(my_dataframe):
+
+    expected = [
+        {
+            'id': '650703',
+            'state': 'EXECUTED',
+            'date': '2023-09-05T11:30:32Z',
+            'amount': '16210',
+            'currency_name': 'Sol',
+            'currency_code': 'PEN',
+            'from': 'Счет 58803664561298323391',
+            'to': 'Счет 39745660563456619397',
+            'description': 'Перевод организации'
+        }
+    ]
+
+    with patch('pandas.read_excel') as mock_read_excel:
+        mock_read_excel.return_value = my_dataframe
+
+        result = read_xlsx_file('test.xlsx')
+
+        mock_read_excel.assert_called_once_with('test.xlsx')
+
+        assert result == expected
+
+
+def test_read_xlsx_file_not_found():
+    with patch('pandas.read_excel', side_effect=FileNotFoundError):
+        result = read_xlsx_file('missing.xlsx')
+
+        assert result == 'Файл не найден'
